ca03d512267526708a6550d743ce1e110c3b76e7
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2025-07-27 18:37:20 +0600

    setup DaisyUI

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
+import Home from './pages/home/home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route element={<App />}>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<Home />} />
         <Route path="/pricing" element={<div> Pricing Page </div>} />
         <Route path="/services" element={<div> Services Page </div>} />
         <Route path="/blogs" element={<div> Blog page </div>} />
